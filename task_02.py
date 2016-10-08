50448b8aa6b1f9967745f7f7b529915c85be2d9d
javedkhan-IS210 <javed.khan@spsmail.cuny.edu>
2016-10-08 21:15:22 +0000

    added and completed task 02

---
+++ task_02.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" A function that tests truthiness and falsiness."""
+
+
+def bool_to_str(bval)
+    """ A function that returns 'yes' or 'no' for the truthiness of the
+        statement.
+
+    Args:
+        bval (mixed): a boolean or boolean-like value to test truthiness.
+
+    Retuns:
+        string (str) : 'Yes" for truthiness, 'No' for falsiness. 
+
+    Examples:
+         
+        >>> task_02.bool_to_str(True)
+            'Yes'
+
+        >>> task 02.bool_to_str('')
+            'No'
+
+    """
+
+    if bval =
+        string = 'Yes'
+
+    else
+        string = 'No'
+
+    return string 
